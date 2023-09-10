fe4f2f7abe4a3fb1a81ed197e18378f3b3ae02e8
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2023-09-10 16:33:14 +0500

    new section

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 2434;
+const port = 80;
 
 const socketIo = require("socket.io");
 const http = require("http");
